c2f668d39a9a2af07650d5bd110c2a4e24c7fc01
AlvaroBernardino <alvarofbernardino@gmail.com>
2025-07-17 02:49:29 -0300

    ajuste no main

--- main/main.py
+++ main/main.py
@@ -1,7 +1,15 @@
 # main.py
+
 import os
+
+# Executa scripts de coleta e análise de dados
 os.system("python scripts/indicadores_economicos.py")
 os.system("python scripts/acoes.py")
 os.system("python scripts/noticias.py")
 os.system("python scripts/agentes_economicos.py")
-os.system("streamlit run streamlit/dashboard.py")
+
+# Obtém a porta que o Azure define como variável de ambiente
+port = os.getenv("PORT", "8000")
+
+# Executa o Streamlit na porta correta, ouvindo em todas as interfaces
+os.system(f"streamlit run streamlit/dashboard.py --server.port={port} --server.address=0.0.0.0")
